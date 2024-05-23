a3a8508df8618b9cd87890f558ffb1ca463cc6b1
daiyechun <1748053887@qq.com>
2024-05-23 22:19:40 +0800

    111修改

---
+++ Assets/AIDemo/UniTaskTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UniTaskTest : MonoBehaviour
+{
+    public Button btn_delay;
+    public Button btn_delay_frame;
+    public Button btn_next_frame;
+    public Button btn_yield;
+    public Button btn_endof_frame;
+    public Button btn_clear;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Tool/AsyncUIFrame/Runtime/Layers/WindowLayer.cs
+++
@@ -1,8 +0,0 @@
-namespace Async.UIFramework
-{
-    public class WindowLayer : UILayer
-    {
-        public override string GetName() => "WindowLayer";
-        public override int GetOrder() => 200;
-    }
-}
