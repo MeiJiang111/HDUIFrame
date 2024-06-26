ef2bd65de7f666b97ee923d1e50919f9e8f99ac0
MeiJiang <1748053887@qq.com>
2024-06-26 15:57:56 +0800

    111

--- Assets/Scripts/HDUIFrame/UI/Login/LoadingPanel.cs
+++ Assets/Scripts/HDUIFrame/UI/Login/LoadingPanel.cs
@@ -1,10 +1,43 @@
 using Async.UIFramework;
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [PanelLayer]
 public class LoadingPanel : UIComponent<LoadingData>
 {
+    public Slider slider;
+    public Text precetText;
 
+    protected override UniTask OnCreate()
+    {
+        return UniTask.CompletedTask;
+    }
+
+    protected override UniTask OnRefresh()
+    {
+        return UniTask.CompletedTask;
+    }
+
+    protected override void OnBind()
+    {
+       
+    }
+
+    protected override void OnUnbind()
+    {
+       
+    }
+
+    protected override void OnShow()
+    {
+
+    }
+
+    protected override void OnHide()
+    {
+
+    }
 }
